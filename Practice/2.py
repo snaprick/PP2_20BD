80b53b1aee732a925c9c59bd84e3515c440793c8
Yerlam <erlan200225@gmail.com>
2021-02-01 16:58:09 +0600

    1

--- Practice/2.py
+++ Practice/2.py
@@ -1,1 +1,9 @@
-print(1234)
+k, n = map(int,input().split())
+dp=[0]*(n+1)
+dp[0]=1
+dp[1]=1
+for i in range(2,n+1):
+    for j in range(1,k+1):
+        if i-j>=0:
+            dp[i]+=dp[i-j]
+print(dp[n])
