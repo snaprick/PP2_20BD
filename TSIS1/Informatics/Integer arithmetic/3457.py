171374862f2344f0919a87712e307264f5d9639e
Yerlam <erlan200225@gmail.com>
2021-02-03 15:40:20 +0600

    arithmetic

---
+++ TSIS1/Informatics/Integer arithmetic/3457.py
@@ -0,0 +1,5 @@
+#The next number for the number 179 is 180.
+#The previous number for the number 179 is 178.
+a = int(input())
+print("The next number for the number",a,"is",a+1,end=".\n")
+print("The previous number for the number",a,"is",a-1,end=".")
