efdf8796ed7886fff152cfc7fa79b77f6ad6e38a
Yerlam <erlan200225@gmail.com>
2021-03-10 14:20:02 +0600

    changes

--- quiz3/D.py
+++ quiz3/D.py
@@ -3,9 +3,9 @@ d = dict()
 s = set()
 a = (str(sys.stdin.read()).split())
 for i in a:
-    d[i] = d.get(i, 1) + 1
+    d[i] = d.get(i, 0) + 1
 for i in d:
-    if(d[i]%2==1):
+    if(d[i]%2==0):
         s.add(i)
 s = sorted(s,key=str)
 for i in s:
